aa2dc6a39beaf6e15a9451192635f5019b519644
Tran Bao Huy <huy23121994@gmail.com>
2020-12-15 14:36:32 +0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ $(document).ready(function () {
   new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
