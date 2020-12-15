a06c05c2af96eb563bc576a3aad3587a25dcae17
Tran Bao Huy <huy23121994@gmail.com>
2020-12-15 20:31:38 +0700

    update

--- src/js/app.js
+++ src/js/app.js
@@ -61,11 +61,3 @@ $(document).ready(function () {
     $("html, body").animate({ scrollTop: 0 }, "slow");
   });
 });
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
